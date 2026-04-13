d328897e270ff28ea5dabd54177dfe1ed0585a12
Tanya <tanya.agrawal061@gmail.com>
2026-04-13 10:25:24 +0530

    Procfile added

--- Med/settings.py
+++ Med/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-d*q71nk+fgwf#!$7^ca*kc5nq%5*qw&!#eh93b(2y3$#v=21_-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
